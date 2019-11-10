d21b182f2f78fd6520311182fc04b57b6435f540
James Stevenson <james.sharpsteen@gmail.com>
2019-11-09 20:38:34 -0600

    Add some extra tokens

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ extern crate regex;
 
 use regex::Regex;
 use std::io;
-// Scanner
-// Scanner struct contains stream
+
+// Scanner/lexer
 
 enum Token {
     // logistical
     NewLine,
+    WhiteSpace,
     // keywords
     List,
     Del,
+    Exit,
+    NoneT,
     // operators
     Plus,
     Minus,
@@ -28,15 +31,14 @@ enum Token {
     Variable(String)
 }
 
-fn initialize_stream(stream: &String) -> &String {
-    return stream;
-}
-
 fn make_patterns_map() -> Vec<(Regex, Token)> {
     let patterns_map = vec![
         (Regex::new(r"^\n").unwrap(), Token::NewLine),
+        (Regex::new(r"^[ ]+").unwrap(), Token::WhiteSpace),
         (Regex::new(r"^list").unwrap(), Token::List),
         (Regex::new(r"^del").unwrap(), Token::Del),
+        (Regex::new(r"^exit").unwrap(), Token::Exit),
+        (Regex::new(r"^None").unwrap(), Token::NoneT),
         (Regex::new(r"^+").unwrap(), Token::Plus),
         (Regex::new(r"^-").unwrap(), Token::Minus),
         (Regex::new(r"^\*\*").unwrap(), Token::Exponent),
@@ -44,11 +46,32 @@ fn make_patterns_map() -> Vec<(Regex, Token)> {
         (Regex::new(r"^/").unwrap(), Token::Divide),
         (Regex::new(r"^\(").unwrap(), Token::LeftParen),
         (Regex::new(r"^\)").unwrap(), Token::RightParen),
-        (Regex::new(r"^=").unwrap(), Token::Equals),
-
+        (Regex::new(r"^=").unwrap(), Token::Equals)
     ];
     return patterns_map;
 }
 
+fn get_next_token(stream: String) -> (String, Token) {
+    // TODO replace with match?
+    if Regex
+    return (
+}
+
+fn update_stream(Regex) {
+
+}
+
 fn main() {
+    //// scanner
+    // initialize stream
+    let mut stream = String::from("");
+    let patterns = make_patterns_map();
+    let token = Token::NoneT;
+
+    // while stream exists:
+    //  run stream against patterns
+    //  retrieve regex match
+    //  from match, get token
+    //  from match, snip stream
+
 }
